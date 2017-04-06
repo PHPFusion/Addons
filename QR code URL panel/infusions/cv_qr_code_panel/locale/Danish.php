73e2a17307bf03201acc1a80995ff86580d2978c
Helmuth Mikkelsen <helmuthm@gmail.com>
2017-04-06 11:08:19 +0200

    Danish.php added
    danish locale file added

---
+++ QR code URL panel/infusions/cv_qr_code_panel/locale/Danish.php
@@ -0,0 +1,42 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: infusions/cv_qr_code_panel/locale/Danish.php
+| Author: Helmuth Mikkelsen (helmuth)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['cvqr_title'] = "QR kode";
+$locale['cvqr_descr'] = "Element, der viser QR kode med link til den nuværende side";
+
+$locale['cvqr_1000'] = "QR-kode indstillinger";
+$locale['cvqr_1001'] = "Du kan ændre disse værdier for bedre visning af QR kode på siderne på din hjemmeside";
+$locale['cvqr_1002'] = "Farver";
+$locale['cvqr_1003'] = "Størrelser";
+$locale['cvqr_1004'] = "Pålidelighed";
+
+$locale['cvqr_1011'] = "Billede";
+$locale['cvqr_1012'] = "Baggrund";
+$locale['cvqr_1016'] = "Firkant størrelse (i pixels)";
+$locale['cvqr_1017'] = "Padding (i firkanter)";
+$locale['cvqr_1021'] = "Korrektionsniveau";
+$locale['cvqr_1022'] = "Lav";
+$locale['cvqr_1023'] = "Mellem";
+$locale['cvqr_1024'] = "Kvalitet";
+$locale['cvqr_1025'] = "Høj";
+
+// Save Settings
+$locale['cvqr_1100'] = "Gem indstillinger";
+
+// Settings Error
+$locale['cvqr_1101'] = "Indstillinger er nu gemt";
+$locale['cvqr_1102'] = "Database opdatering fejlede: Indstillinger blev ikke gemt!";

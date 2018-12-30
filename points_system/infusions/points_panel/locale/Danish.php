8658326f5d813d30ade618a92edb2d62adb00865
Helmuth Mikkelsen <helmuthm@gmail.com>
2018-12-30 12:46:01 +0100

    added Danish.php
    points_system/infusions/points_panel/locale/Danish.php

---
+++ points_system/infusions/points_panel/locale/Danish.php
@@ -0,0 +1,47 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: locale/Danish/infusions/points_panel/locale/Danish.php
+| Author: Helmuth Mikkelsen (helmuth)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['PNT_I01'] = "Pointsystem";
+$locale['PNT_I02'] = "Brugerpointsystem";
+$locale['PNT_I03'] = "Pointbonus";
+$locale['PNT_P01'] = "Pointsystem";
+
+$locale['PONT_001'] = "Ny registrering. %s point blev tildelt ved oprettelse. Yderligere point vil blive tildelt hver %s time.";
+$locale['PONT_002'] = "Point:";
+$locale['PONT_003'] = "Placeret:";
+$locale['PONT_004'] = "N¾ste ¿gning:<p>%s</p>";
+$locale['PONT_005'] = "Dato";
+$locale['PONT_006'] = "Point";
+$locale['PONT_007'] = "Mod";
+$locale['PONT_010'] = "Daglig credit.";
+$locale['PONT_011'] = "Pointsystemer er i ¿jeblikket deaktiveret.";
+$locale['PONT_012'] = "Ingen indl¾g.";
+
+$locale['PONT_100'] = "Pointsystem";
+$locale['PONT_101'] = "Indstillinger";
+$locale['PONT_102'] = "Dagbog";
+
+$locale['PONT_110'] = "Pointsystem";
+$locale['PONT_111'] = "Aktiver eller deaktiver systemet";
+$locale['PONT_112'] = "F¿rste point";
+$locale['PONT_113'] = "F¿rste log ind tildelte point";
+$locale['PONT_114'] = "Point¿gning";
+$locale['PONT_115'] = "Point¿gning (dag)";
+$locale['PONT_116'] = "Dag point";
+$locale['PONT_117'] = "Daglig point ¿gning";
+$locale['PONT_118'] = "Emner pr. side";
+$locale['PONT_300'] = "Indstillinger blev opdateret";

588e27453b1dfd2921e369e2f21b8fa92f7ce04a
RobiNN <kelcakrobo@gmail.com>
2020-11-15 16:38:20 +0100

    Add Artemis theme

---
+++ AdminThemes/Artemis/locale/Slovak.php
@@ -0,0 +1,8 @@
+<?php
+$locale['spotlight'] = "Spotlight";
+$locale['spotlight_search'] = "Hľadať aplikácie";
+$locale['copyright'] = "Copyright &copy; ";
+$locale['render_engine'] = "Render Engine";
+$locale['artemis_admin'] = "Artemis PHP-Fusion 9";
+$locale['admin_apps'] = "Aplikácie";
+$locale['admin_collapse'] = "Zbaliť menu";

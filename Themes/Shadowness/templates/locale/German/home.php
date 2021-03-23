67a312bf5a29f1365f7a85e9beda0013fd6b3bca
RobiNN <kelcakrobo@gmail.com>
2021-03-23 13:50:40 +0100

    Add locales

---
+++ Themes/Shadowness/templates/locale/German/home.php
@@ -0,0 +1,20 @@
+<?php
+/*-------------------------------------------------------+
+| PHPFusion Content Management System
+| Copyright (C) PHP Fusion Inc
+| https://phpfusion.com/
++--------------------------------------------------------+
+| Filename: home.php
+| Author: sala99
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['sh_0100'] = 'Neueste';
+$locale['sh_0101'] = 'Am beliebtesten';
+$locale['sh_0102'] = 'Es sind keine Fotos anzuzeigen';
---
+++ Themes/Shadowness/templates/locale/German/login.php
@@ -0,0 +1,23 @@
+<?php
+/*-------------------------------------------------------+
+| PHPFusion Content Management System
+| Copyright (C) PHP Fusion Inc
+| https://phpfusion.com/
++--------------------------------------------------------+
+| Filename: login.php
+| Author: sala99
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['sh_0100'] = 'Einloggen in ';
+$locale['sh_0101'] = 'Protokollierte Benutzer können exklusive Inhalte anzeigen, Inhalte austauschen und an der Konversation teilnehmen!';
+$locale['sh_0103'] = 'Benutzer Name';
+$locale['sh_0104'] = 'Password';
+$locale['global_103'] = 'Eingeloggt bleiben';
+$locale['global_104'] = 'Login';

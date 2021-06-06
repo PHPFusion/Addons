4905f417fec3e6df960a4eade2820396f3f1cdd1
RobiNN <kelcakrobo@gmail.com>
2021-06-06 19:01:40 +0200

    Update Admin theme SDK

--- SDK/DashboardName/acp_theme.php
+++ SDK/DashboardName/acp_theme.php
@@ -18,10 +18,10 @@
 +--------------------------------------------------------*/
 defined('IN_FUSION') || exit;
 
-use \PHPFusion\Admins;
+use PHPFusion\Admins;
 
-define('BOOTSTRAP', TRUE);
-define('FONTAWESOME', TRUE);
+const BOOTSTRAP = TRUE;
+const FONTAWESOME = TRUE;
 
 function render_admin_panel() {
     $locale = fusion_get_locale();
@@ -184,7 +184,7 @@ function render_admin_dashboard() {
         $html = fusion_get_function('opentable', $admin_title);
         $html .= '<div class="row">';
             if (count($admin_icons['data']) > 0) {
-                foreach ($admin_icons['data'] as $i => $data) {
+                foreach ($admin_icons['data'] as $data) {
                     $html .= '<div class="icon-wrapper col-xs-6 col-sm-2 col-md-2 col-lg-2">';
                         $html .= '<a class="text-center" href="'.$data['admin_link'].$aidlink.'">';
                             $html .= '<img class="display-block" src="'.get_image('ac_'.$data['admin_rights']).'" alt="'.$data['admin_title'].'"/>';
@@ -311,7 +311,7 @@ function render_admin_dashboard() {
                 }
 
                 if (!empty($modules)) {
-                    foreach ($modules as $name => $module) {
+                    foreach ($modules as $module) {
                         $html .= '<div class="col-xs-'.$grid['mobile'].' col-sm-'.$grid['tablet'].' col-md-'.$grid['laptop'].' col-lg-'.$grid['desktop'].'">';
                             $html .= fusion_get_function('openside', '');
                             $html .= '<strong class="text-uppercase">'.$module['title'].' '.$locale['258'].'</strong>';
@@ -350,8 +350,7 @@ function render_admin_dashboard() {
                                                 $html .= '<strong>'.(!empty($comment_data['user_id']) ? profile_link($comment_data['user_id'], $comment_data['user_name'], $comment_data['user_status']) : $comment_data['comment_name']).' </strong>';
                                                 $html .= $locale['273'].' <a href="'.sprintf($link_type[$comment_data['comment_type']], $comment_data['comment_item_id']).'"><strong>'.$comments_type[$comment_data['comment_type']].'</strong></a> ';
                                                 $html .= timer($comment_data['comment_datestamp']).'<br/>';
-                                                $comment = trimlink(strip_tags(parse_textarea($comment_data['comment_message'], FALSE, TRUE)), 130);
-                                                $html .= '<span class="text-smaller">'.parse_textarea($comment, TRUE, FALSE).'</span>';
+                                            $html .= '<span class="text-smaller">'.trimlink(strip_tags(parse_textarea($comment_data['comment_message'], FALSE)), 130).'</span>';
                                             $html .= '</div>';
                                         }
                                     }
@@ -427,7 +426,7 @@ function render_admin_dashboard() {
                 if (checkrights('I')) {
                     $html .= '<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">';
                         $html .= '<div id="infusions">';
-                            $html .= fusion_get_function('openside', '<strong class="text-uppercase">'.$locale['283'].'</strong><span class="pull-right badge">'.number_format((int)$infusions_count).'</span>');
+                            $html .= fusion_get_function('openside', '<strong class="text-uppercase">'.$locale['283'].'</strong><span class="pull-right badge">'.number_format($infusions_count).'</span>');
                                 $content = '';
                                 if ($infusions_count > 0) {
                                     if (!empty($global_infusions)) {
